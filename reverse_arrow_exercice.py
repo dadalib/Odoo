9eb63010999230245101cc221357033ed004b2c8
darwinlibioulle <darwinlibioulle@penguin>
2022-03-14 14:58:28 +0100

    solution using an buffer

--- reverse_arrow_exercice.py
+++ reverse_arrow_exercice.py
@@ -14,31 +14,39 @@ import sys
 #  1. STRING_ARRAY arr
 #  2. 2D_INTEGER_ARRAY operations
 
-def performOperations(arr, operations):
+def performOperations(arr, list_operations):
     """Calculate how many steps you need for your target 
     
     Parameters
     ----------
     arr : list
-    operationn : list
+    list_operationn : list
 
     Returns:
     --------
-    new_arr : list
+    arr : list
     """
+    # build empty list
+    new_arr = []
 
-    # loop the list until it match the target
-    while tools[startIndex] != target:
-        # incriment the index and steps counter 
-        if tools[startIndex] != target:
-            startIndex += 1
-            steps += 1
+    # loop array according to the operations
+    for operations in list_operations:
+        new_arr = arr.copy()
+        # remove index from copy list of all list
+        new_arr.pop(arr.index(arr[operations[1]]))
+        new_arr.pop(arr.index(arr[operations[0]]))
+        # insert values with operations and buffer values
+        new_arr.insert(operations[0],arr[operations[1]])
+        new_arr.insert(operations[1],arr[operations[0]])
+        # rebuild buffer
+        arr = new_arr
 
-        
-    print ("steps",steps)
-    # return steps
-    return steps  
+    print("final",new_arr)
+    return (new_arr) 
+
 
 if __name__ == '__main__':
-    tools = [5,3,2,1,3]
-    startIndex = [[0,1],[1,3]]
+    arr = [5,3,2,1,3]
+    list_operations = [[0,1],[1,3]]
+
+    performOperations(arr, list_operations)
