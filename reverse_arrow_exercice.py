4c4121798dfeda0bb7dbf35ca4e973189f313031
darwinlibioulle <darwinlibioulle@penguin>
2022-03-14 15:00:05 +0100

    black

--- reverse_arrow_exercice.py
+++ reverse_arrow_exercice.py
@@ -14,9 +14,10 @@ import sys
 #  1. STRING_ARRAY arr
 #  2. 2D_INTEGER_ARRAY operations
 
+
 def performOperations(arr, list_operations):
-    """Calculate how many steps you need for your target 
-    
+    """Calculate how many steps you need for your target
+
     Parameters
     ----------
     arr : list
@@ -36,17 +37,17 @@ def performOperations(arr, list_operations):
         new_arr.pop(arr.index(arr[operations[1]]))
         new_arr.pop(arr.index(arr[operations[0]]))
         # insert values with operations and buffer values
-        new_arr.insert(operations[0],arr[operations[1]])
-        new_arr.insert(operations[1],arr[operations[0]])
+        new_arr.insert(operations[0], arr[operations[1]])
+        new_arr.insert(operations[1], arr[operations[0]])
         # rebuild buffer
         arr = new_arr
 
-    print("final",new_arr)
-    return (new_arr) 
+    print("final", new_arr)
+    return new_arr
 
 
-if __name__ == '__main__':
-    arr = [5,3,2,1,3]
-    list_operations = [[0,1],[1,3]]
+if __name__ == "__main__":
+    arr = [5, 3, 2, 1, 3]
+    list_operations = [[0, 1], [1, 3]]
 
     performOperations(arr, list_operations)
